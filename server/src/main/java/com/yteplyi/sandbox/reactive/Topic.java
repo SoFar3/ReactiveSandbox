285c2325f9263250ab6b5692ba2571c753fe185d
sofar03 <yteplij2@gmail.com>
2022-07-19 18:11:19 +0300

    Add simple endpoint that prints message from path params

---
+++ server/src/main/java/com/yteplyi/sandbox/reactive/Topic.java
@@ -0,0 +1,13 @@
+package com.yteplyi.sandbox.reactive;
+
+public enum Topic {
+
+    NOTIFICATION_TOPIC("notification_topic");
+
+    public final String name;
+
+    Topic(String name) {
+        this.name = name;
+    }
+
+}
